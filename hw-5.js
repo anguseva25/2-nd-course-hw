3f50fc346c8ab408963e9cd49147872a5f92b1e3
anguseva25 <annaguseva97@gmail.com>
2024-01-29 22:30:30 +0300

    done ex. 1-3

---
+++ hw-5.js
@@ -0,0 +1,27 @@
+//Задание 1
+function interval(a, b) {
+    return (a => b) ? b : a;
+}
+
+let result1 = console.log(interval(8, 4));
+let result2 = console.log(interval(6, 6));
+
+//Задание 2
+function backNumberType(n) {
+    return (n % 2 == 0) ? ('число четное') : ('число нечетное')
+}
+let chance1 = console.log(backNumberType(15));
+let chance2 = console.log(backNumberType(26));
+
+//Задание 3.1
+function value(n) {
+    return Math.pow(n, 2) ? n ** 2 : 'none info'
+}
+let question = console.log(value(8));
+
+//Задание 3.2
+function value(n) {
+    return Math.sqrt(n)
+}
+let question1 = console.log(value(121));
+
