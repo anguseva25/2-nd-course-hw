3b8813e10c51f1ba34920349bbda7b91d121cdd5
anguseva25 <annaguseva97@gmail.com>
2024-02-02 21:29:32 +0300

    finished hw-5

--- script-game.js
+++ script-game.js
@@ -1,43 +1,46 @@
 // Игра 1
-let monthNumber = 12;
-switch (monthNumber) {
-    case 12:
-    case 1:
-    case 2:
-        console.log('зима');
-        break;
-    case 3:
-    case 4:
-    case 5:
-        console.log('весна');
-        break;
-    case 6:
-    case 7:
-    case 8:
-        console.log('лето');
-        break;
-    case 9:
-    case 10:
-    case 11:
-        console.log('осень');
-        break;
-    default:
-        console.log('нет такого месяца');
-        break;
+const monthNumber = prompt('Введите число от 1 до 12, и я назову время года')
+function guessSeason(){
+    if (monthNumber === 12 || monthNumber === 1 || monthNumber === 2) {
+        return 'Это зима"';
+    } else if (monthNumber === 3 || monthNumber === 4 || monthNumber === 5) {
+        return 'Это весна"';
+    } else if (monthNumber === 6 || monthNumber === 7 || monthNumber === 8) {
+        return 'Это лето"';
+    } else if (monthNumber === 9 || monthNumber === 10 || monthNumber === 11) {
+        return 'Это осень"';
+    }
 }
 
 // Игра 2
-let password = 'пароль';
-let answer = prompt('Введите пароль');
-// let result = password === $пароль ? alert ('Пароль введен верно') : alert ('Пароль введен неверно');
-if (answer === password) {
-    console.log('пароль введен верно');
-} else {
-    console.log('пароль введен неверно');
+confirm(`Запомните слова: "письмо", "шуфлятка", "одеколон", "цветы", "радиатор", "корги", "сапоги"`);
+let r = prompt('Введите пятое слово')
+function rememberWords(r) {
+    return (r === 'радиатор' || r === 'Радиатор') ? 'хорошая память! Ты молодец!' : "Похоже что слово не запомнилось"
 }
+console.log(alert('Правильный ответ "радиатор". Спасибо за игру'));
+
 
 // Игра 3
-function ask(r) {
-    return (r !== 0 && r <= 12) ? ('«Привет, друг!»') : ('Добро пожаловать')
+let riddleQuestion = prompt('Что посмотреть с закрытыми глазами?')
+let answerUser;
+function riddle(riddleQuestion, answerUser) {
+    if (answer === 'сон' || answer === 'сны' ) {
+        console.log('Молодец! Угадал!'); 
+     } else {
+        console.log('Не угадал');
 }
-let result3 = console.log(ask(prompt('Введите возраст')));
+}
+function riddle(question, answer) {
+    let answerUser = prompt(question);
+
+    if (answerUser === answer){
+        console.log('угадал!');
+    } else {
+        console.log('не угадал');
+    }
+}
+
+riddle ('Что посмотреть с закрытыми глазами?', 'сон');
+riddle ('Куда я, туда и она', 'тень');
+
