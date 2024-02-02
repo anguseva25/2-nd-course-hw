60f88373f632aeeef66d8786d36ba335056caae0
anguseva25 <annaguseva97@gmail.com>
2024-02-03 00:19:41 +0300

    done ex.1-3

---
+++ hw-6.js
@@ -0,0 +1,46 @@
+// Задание 1
+const numbs = [1, 5, 4, 10, 0, 3]
+for (let i = 0; i < numbs.length; i++) {
+    if (numbs[i] === 10) break;
+    console.log(numbs[i]);
+}
+
+// Задание 2
+const nummer = [1, 5, 4, 10, 0, 3]
+console.log(nummer.indexOf(4))
+
+// Задание 3
+const space = [1, 3, 5, 10, 20]
+result = console.log(space.join(" "))
+
+// Задание 4
+const t = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+];
+
+
+const t = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+
+// Задание 5
+const addNumbers = [1, 1, 1]
+addNumbers.push(2, 2, 2)
+console.log(addNumbers)
+
+// Задание 6
+let u = [9, 8, 7, 'a', 6, 5]
+let e = u.filter(item === Number(item));
+console.log(e);
+
+
+let x = 'привет=как=дела';
+x = x.split('=');
+
+// Задание 11 (нужен reduce для суммы массива видео 5)
+
+// Задание 11 (нужен map видео 5)
+let mass = [178, 53, 42, 960, 4, 21, 45, 67, 98, 171]
+function price {
+    return (item)
+}
