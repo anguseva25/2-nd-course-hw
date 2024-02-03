8e024a94b4bd2363cf0c0b6722a26d70fdf85e20
anguseva25 <annaguseva97@gmail.com>
2024-02-03 12:17:40 +0300

    corrected js file with games

--- script-game.js
+++ script-game.js
@@ -13,34 +13,38 @@ function guessSeason(){
 }
 
 // Игра 2
-confirm(`Запомните слова: "письмо", "шуфлятка", "одеколон", "цветы", "радиатор", "корги", "сапоги"`);
-let r = prompt('Введите пятое слово')
 function rememberWords(r) {
+    confirm(`Запомните слова: "письмо", "шуфлятка", "одеколон", "цветы", "радиатор", "корги", "сапоги"`);
+    let r = String(prompt('Введите пятое слово'))
     return (r === 'радиатор' || r === 'Радиатор') ? 'хорошая память! Ты молодец!' : "Похоже что слово не запомнилось"
 }
-console.log(alert('Правильный ответ "радиатор". Спасибо за игру'));
+alert('Правильный ответ "радиатор". Спасибо за игру');
 
 
 // Игра 3
-let riddleQuestion = prompt('Что посмотреть с закрытыми глазами?')
-let answerUser;
-function riddle(riddleQuestion, answerUser) {
-    if (answer === 'сон' || answer === 'сны' ) {
-        console.log('Молодец! Угадал!'); 
-     } else {
-        console.log('Не угадал');
-}
-}
-function riddle(question, answer) {
-    let answerUser = prompt(question);
 
-    if (answerUser === answer){
-        console.log('угадал!');
-    } else {
-        console.log('не угадал');
-    }
-}
+//                       Вариант 1
+// function riddle(riddleQuestion, answerUser) {
+//     let riddleQuestion = prompt('Что посмотреть с закрытыми глазами?')
+//     let answerUser
+//     if (answer === 'сон' || answer === 'сны' ) {
+//         console.log(alert('Молодец! Угадал!')); 
+//      } else {
+//         console.log(alert('Не угадал'));
+// }
+// }
+//                        Вариант 2
+// function riddle(question, answer) {
+//     let answerUser = prompt(question);
+
+//     if (answerUser === answer){
+//         console.log('угадал!');
+//     } else {
+//         console.log('не угадал');
+//     }
+// }
+
+//                       Вариант 3
 
 riddle ('Что посмотреть с закрытыми глазами?', 'сон');
 riddle ('Куда я, туда и она', 'тень');
-
