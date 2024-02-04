7ee015a5a976f07537f83df709f436aa07d9e876
anguseva25 <annaguseva97@gmail.com>
2024-02-04 13:39:27 +0300

    done ex.4

--- hw-6.js
+++ hw-6.js
@@ -14,27 +14,18 @@ const space = [1, 3, 5, 10, 20]
 result = console.log(space.join(" "))
 
 // Задание 4
-const t = [
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 1]
-];
-
-
-let arr1 = [1, 1, 1];
-let mainArray
-for (let i = 0; i < 1; i++)
+const t = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+const arr = []
+for (let i = 0; i < 3; i++)
 {
 arr[i] = [];
-for (let j = 0; j <= i; j++)
+for (let j = 0; j < 3; j++)
 {
-arr[i][j] = j + 1;
+arr[i][j] = 1;
 }
 }
 console.log(arr);
 
-
-const t = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 
 // Задание 5
 const addNumbers = [1, 1, 1]
