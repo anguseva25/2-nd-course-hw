db85c5135de3b1a599d783e58e9ddb878b9cd7b5
anguseva25 <annaguseva97@gmail.com>
2024-02-05 16:27:32 +0300

    done ex 7-8

--- hw-6.js
+++ hw-6.js
@@ -16,16 +16,13 @@ result = console.log(space.join(" "))
 // Задание 4
 const t = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 const arr = []
-for (let i = 0; i < 3; i++)
-{
-arr[i] = [];
-for (let j = 0; j < 3; j++)
-{
-arr[i][j] = 1;
-}
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+    for (let j = 0; j < 3; j++) {
+        arr[i][j] = 1;
+    }
 }
 console.log(arr);
-
 
 // Задание 5
 const addNumbers = [1, 1, 1]
@@ -33,10 +30,32 @@ addNumbers.push(2, 2, 2)
 console.log(addNumbers)
 
 // Задание 6
-let u = [9, 8, 7, 'a', 6, 5]
-let e = u.filter(item === Number(item));
-console.log(e);
+let arrayOrigine = [9, 8, 7, 'a', 6, 5]
+const newArray = arrayOrigine.splice(3, 1)
+console.log(arrayOrigine);
 
+// Задание 7
+const array = [9, 8, 7, 6, 5]
+const checkedArray = array.includes(Number(prompt('угадайте число от 1 до 10, которое содержится в загаданном массиве')));
+console.log(alert(checkedArray))
+
+
+// Задание 8
+let alphabet = 'abcdef'
+let newAlphabet = alphabet.split('')
+console.log(newAlphabet);
+let newAlphabet2 = newAlphabet.sort()
+console.log(newAlphabet2);
+let reverseAlphabet = newAlphabet2.reverse()
+console.log(reverseAlphabet);
+let wordNewAlphabet = reverseAlphabet.join('')
+console.log(wordNewAlphabet);
+
+// Задание 9
+// Задание 10
+// Задание 11
+// Задание 12
+// Задание 13
 
 let x = 'привет=как=дела';
 x = x.split('=');
@@ -48,6 +67,5 @@ console.log(result);
 
 // Задание 11 (нужен map видео 5)
 let mass = [178, 53, 42, 960, 4, 21, 45, 67, 98, 171]
-function price {
+function price 
     return (item)
-}
